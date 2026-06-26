b63abb692d10baed95a770f1d513a65e99b8de99
Yogesh Mudgal <yogeshmudgal@Yogeshs-MacBook-Air.local>
2026-06-26 20:16:52 +0530

    code written

--- yogesh.py
+++ yogesh.py
@@ -1,1 +1,24 @@
 #File created by Yogesh -- 26-06-2026
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "No common language"
+
+    @staticmethod
+    def category():
+        return "Living Being"
+
+
+class Dog(Animal):
+    def speak(self):        
+        return f"{self.name} barks..."
+
+
+a = Animal("Common")
+d = Dog("Moti")
+
+print(a.speak())                  
+print(d.speak())         
